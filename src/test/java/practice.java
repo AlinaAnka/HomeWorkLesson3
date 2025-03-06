1f37ded4516304cb81e7c33a582561f4d251cf7e
Alina K <alina885@mail.ru>
2025-03-06 10:45:16 +0300

    added practice

---
+++ src/test/java/practice.java
@@ -0,0 +1,58 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class practice {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Alina");
+        $("#lastName").setValue("Kovrigina");
+        $("#userEmail").setValue("alina885@mail.ru");
+        $("label[for='gender-radio-2']").click();
+        $("#userNumber").setValue("1122334455");
+        $("#dateOfBirthInput").click();
+        $("select.react-datepicker__year-select").selectOption("1905");
+        $("select.react-datepicker__month-select").selectOption("April");
+        $("div.react-datepicker__day--005").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("label[for='hobbies-checkbox-1']").click();
+        $("#uploadPicture").uploadFile(new File("/Users/alina/Desktop/picture.png"));
+        $("#currentAddress").setValue("Moscow55");
+        $("#react-select-3-input").setValue("Haryana").pressEnter();
+        $("#react-select-4-input").setValue("Karnal").pressEnter();
+        $("#submit").click();
+
+        $("div.table-responsive").shouldHave(
+                text("Alina Kovrigina"),
+                text("alina885@mail.ru"),
+                text("Female"),
+                text("1122334455"),
+                text("05 April,1905"),
+                text("Maths"),
+                text("Sports"),
+                text("picture.png"),
+                text("Moscow55"),
+                text("Haryana Karnal"));
+        ;
+
+
+    }
+
+
+}

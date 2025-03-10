5fc38435f2b9d49494da12daba80fa26ff7dee73
Alina K <alina885@mail.ru>
2025-03-10 19:29:57 +0300

    fix

--- src/test/java/PracticeTest.java
+++ src/test/java/PracticeTest.java
@@ -34,51 +34,17 @@ public class PracticeTest {
         $("#react-select-4-input").setValue("Karnal").pressEnter();
         $("#submit").click();
 
-        $("div.table-responsive").shouldHave(
-                text("Alina Kovrigina"),
-                text("alina885@mail.ru"),
-                text("Female"),
-                text("1122334455"),
-                text("05 April,1905"),
-                text("Maths"),
-                text("Sports"),
-                text("picture.png"),
-                text("Moscow55"),
-                text("Haryana Karnal"));
+        $("div.table-responsive").shouldHave(text("Alina Kovrigina"));
+        $("div.table-responsive").shouldHave(text("alina885@mail.ru"));
+        $("div.table-responsive").shouldHave(text("Female"));
+        $("div.table-responsive").shouldHave(text("1122334455"));
+        $("div.table-responsive").shouldHave(text("05 April,1905"));
+        $("div.table-responsive").shouldHave(text("Maths"));
+        $("div.table-responsive").shouldHave(text("Sports"));
+        $("div.table-responsive").shouldHave(text("picture.png"));
+        $("div.table-responsive").shouldHave(text("Moscow55"));
+        $("div.table-responsive").shouldHave(text("Haryana Karnal"));
     }
-
-    @Test
-    void fillFormTestNegative() {
-        open("/automation-practice-form");
-        $("#firstName").setValue("Alina");
-        $("#lastName").setValue("Kovrigina");
-        $("#userEmail").setValue("alina885@mail.ru");
-        $("label[for='gender-radio-2']").click();
-        $("#userNumber").setValue("1122334455");
-        $("#dateOfBirthInput").click();
-        $("select.react-datepicker__year-select").selectOption("1905");
-        $("select.react-datepicker__month-select").selectOption("April");
-        $("div.react-datepicker__day--005").click();
-        $("#subjectsInput").setValue("Math").pressEnter();
-        $("label[for='hobbies-checkbox-1']").click();
-        $("#uploadPicture").uploadFromClasspath(("picture.png"));
-        $("#currentAddress").setValue("Moscow55");
-        $("#react-select-3-input").setValue("Haryana").pressEnter();
-        $("#react-select-4-input").setValue("Karnal").pressEnter();
-        $("#submit").click();
-
-        $("div.table-responsive").shouldHave(
-                text("Alina Kovrigina1"),
-                text("alina885@mail.ru"),
-                text("Female"),
-                text("1122334455"),
-                text("05 April,1905"),
-                text("Maths"),
-                text("Sports"),
-                text("picture.png"),
-                text("Moscow55"),
-                text("Haryana Karnal"));
-    }
-
 
 }
+
